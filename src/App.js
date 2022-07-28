42a3c190f199dcf7c936fc2d81a6eef062fcb42e
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-07-28 10:12:55 -0500

    Get started with Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import './App.css';
-import Character from './components/Character';
-import Header from './components/Header';
-import styled from 'styled-components'
+import "./App.css";
+
+import Character from "./components/Character";
+import Header from "./components/Header";
+
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Character/>
+      <Header />
+      <Character />
+      <DisplayCharacters />
     </div>
   );
+}
+
+const GET_CHARACTERS = gql`
+  query {
+    characters {
+      results {
+        id
+        name
+        image
+        species
+        status
+        type
+        gender
+        origin {
+          name
+        }
+        location {
+          name
+        }
+        created
+      }
+    }
+  }
+`;
+
+function DisplayCharacters() {
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.characters.results.map(({ id, name, image }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${image}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{name}</p>
+      <br />
+    </div>
+  ));
 }
 
 export default App;
